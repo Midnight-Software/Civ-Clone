32e333aad89ef2e0a64b18bc44274cfc79a87afe
bluesawdust <tbalzer2012@gmail.com>
2015-07-25 10:58:58 -0500

    C (clang)(cygwin) setup.

---
+++ c-try/main.c
@@ -0,0 +1,15 @@
+//       AUTHOR : THOMAS BALZER
+//CREATION DATE : 12:17 AM JULY 25 SATURDAY 2015
+
+//This only defines this for SFML.
+#define _WIN32
+
+#include <stdio.h>
+#include <SFML/Audio.h>
+#include <SFML/Graphics.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Hallo there.");
+  return 0;
+}
